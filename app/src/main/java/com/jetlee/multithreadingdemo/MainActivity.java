a2adea9b74a2fc42d0fdb04e91d57757dcd9e902
jetLee92 <940077208@qq.com>
2018-09-04 04:02:05 +0800

    RxJava源码分析

--- app/src/main/java/com/jetlee/multithreadingdemo/MainActivity.java
+++ app/src/main/java/com/jetlee/multithreadingdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.jetlee.multithreadingdemo;
 
+import android.os.Bundle;
 import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.internal.schedulers.NewThreadWorker;
 
 public class MainActivity extends AppCompatActivity {
     Handler handler;
@@ -21,20 +26,45 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.text);
 
         handler = new MyHandler();
-
         handler.sendMessage(new Message());
-        Log.e("1000","看看吧");
+//        NewThreadWorker
+        Single.just(1)
+//                .subscribeOn()
+                .map(new Function<Integer, String>() {
+                    @Override
+                    public String apply(Integer integer) {
+                        return String.valueOf(integer);
+                    }
+                })
+                .subscribe(new SingleObserver<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(String s) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                });
+        Log.e("1000", "看看吧");
+//        Observable.map()
+
     }
 
     private class MyHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            Log.e("1000","来了位");
+            Log.e("1000", "来了位");
             textView.setText("来到了Handler");
         }
     }
-
 
 
 }
